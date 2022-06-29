65bbfdbd2fc5cdc35d2daf3b5d23764533dc957f
Roman Filippov <107063780+Roman-Baraban@users.noreply.github.com>
2022-06-29 19:59:06 +0300

    2906221959

---
+++ Sachlav_Lesson1/src/home_tasks/jubilyNumbers.java
@@ -0,0 +1,21 @@
+package home_tasks;
+
+public class jubilyNumbers {
+    public static void main(String[] args) {
+        int i = 0;
+        while (i<=1000){
+            System.out.println(i);
+            i+=5;
+        }
+// Или так
+        i = 0;
+        while(i<=1000){
+            if (i%5==0){
+                System.out.println(i);
+            }
+            i++;
+
+        }
+             
+    }
+}
